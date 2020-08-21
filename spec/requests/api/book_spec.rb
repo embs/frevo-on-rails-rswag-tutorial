4e9211c2a485de8e96d370ac69c6d7409db1bd79
Matheus Santana <matheus@guava.software>
2020-08-21 09:11:57 -0300

    Fix path in specs

--- spec/requests/api/book_spec.rb
+++ spec/requests/api/book_spec.rb
@@ -1,7 +1,7 @@
     require 'swagger_helper'
 
     describe 'Books API' do
-        path '/books' do
+        path '/api/books' do
             get 'List all available books' do
                 produces 'application/json'
                 response '200', 'books listed' do
